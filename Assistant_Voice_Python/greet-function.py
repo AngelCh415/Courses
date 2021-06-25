1aff9759574cadacc2a4df27565007aa8849d3cd
AngelCh415 <angelchavezmorones@gmail.com>
2021-06-25 15:14:53 -0500

    Greet-Function

---
+++ Assistant_Voice_Python/greet-function.py
@@ -0,0 +1,34 @@
+import pyttsx3 #pip install pyttsx3
+import datetime
+import numpy
+
+engine = pyttsx3.init() #Init
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id) 
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def time():
+    Time = datetime.datetime.now().strftime("%I:%M:%S")
+    speak("The hour is " +Time)
+
+def date():
+    Month = numpy.array (["Januery", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"])
+    year = int(datetime.datetime.now().year) #Get the Year
+    month = int(datetime.datetime.now().month) #Get the Month
+    day = int(datetime.datetime.now().day) #Get the Day
+    speak("Today is ")
+    speak(day)
+    speak(Month[month-1])
+    speak(year)
+
+def wishme():
+    speak("Welcome back sir !")
+    time()
+    date()
+    speak("How I can help you?")
+
+wishme()
